6fcf89cd9d8fd784b1f158ed78c4390853b96ea0
ahmad786writes <123527766+ahmad786writes@users.noreply.github.com>
2023-01-25 09:25:38 +0500

    Update example.py

--- example.py
+++ example.py
@@ -28,15 +28,15 @@ for x,fileName in enumerate(images_path):
     mat_img = cv2.resize(mat_img, (512, 512), interpolation=cv2.INTER_CUBIC)
     mat_img = cv2.cvtColor(mat_img, cv2.COLOR_RGB2BGR)
     # Setting gender parameters, 0 means male while 1 means female
-    combine_model.sex = 1
+    combine_model.sex = 0
     # Setting refinement parameters, can be float between 0 and 1
     # the closer to 0, result will stick more to your sketch
     # the closer to 1, result will be more refined and smoothed
-    combine_model.part_weight['eye1'] = 1
-    combine_model.part_weight['eye2'] = 1
-    combine_model.part_weight['nose'] = 1
-    combine_model.part_weight['mouth'] = 1
-    combine_model.part_weight[''] = 1
+    combine_model.part_weight['eye1'] = 0
+    combine_model.part_weight['eye2'] = 0
+    combine_model.part_weight['nose'] = 0
+    combine_model.part_weight['mouth'] = 0
+    combine_model.part_weight[''] = 0
     combine_model.predict_shadow(mat_img)
     # This is where model writes result images
     cv2.imwrite('./results/' + 'res'+ str(x + 1) +'_male.jpg',cv2.cvtColor(combine_model.generated, cv2.COLOR_BGR2RGB))
